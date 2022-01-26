82e13739b5aa01333577cb449d01fb7d68f75da8
kuladip <kuladipyadav@gmail.com>
2022-01-25 17:07:15 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# primeng-inputmask-demo-ugxbe3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/primeng-inputmask-demo-ugxbe3)
---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {MenuItem} from 'primeng/api';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent { 
+    val1: string;
+
+    val2: string;
+
+    val3: string;
+
+    val4: string;
+
+    val5: string;
+    
+    val6: string;
+}
